a98f7b358c1a5c3944980b54d7d2524902e629d6
unknown <mossysm96@gmail.com>
2023-04-29 17:04:40 -0500

    need to finish Intermediate

--- javaIntermediate.js
+++ javaIntermediate.js
@@ -347,3 +347,29 @@
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Number 8...
+// // The following code creates a new Map object for storing names beginning with A, B, or C
+// // with their phone numbers.
+const phoneBookABC = new Map() //an empty map to begin with
+phoneBookABC.set('Annabelle', '0412312343')
+phoneBookABC.set('Barry', '0433221117')
+phoneBookABC.set('Caroline', '0455221182')
+// console.log(phoneBookABC)
+
+
+// // a) Create a new phoneBookDEF Map to store names beginning with D, E or F
+const phoneBookDEF = new Map()
+phoneBookABC.set('David', '0415689451')
+phoneBookABC.set('Edgar', '0510584115')
+phoneBookABC.set('Francine', '0136645920')
+
+// // b) Initialise the contents of phoneBookDEF by passing in an array of keys/values //I think I already did this above
+
+
+// // c) Update the phone number for Caroline
+phoneBookABC.set("Caroline", 0123654789)
+console.log(phoneBookDEF) //WTF??????.... Come back later...
+
+// // d) Write a function printPhoneBook(contacts) that prints the names and phone
+// // numbers in the given Map
+// // e) Combine the contents of the two individual Maps into a single phoneBook Map
+// // f) Print out the full list of names in the combined phone book
