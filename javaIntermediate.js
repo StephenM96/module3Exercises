2712ec28c786c01604094915610e12d9a2738c35
unknown <mossysm96@gmail.com>
2023-04-29 16:21:36 -0500

    working through intermediate... still

--- javaIntermediate.js
+++ javaIntermediate.js
@@ -146,101 +146,188 @@
 // // Decimal number operations in JavaScript can lead to unexpected results, as in the
 // // following:
 
-let twentyCents = 0.20
-let tenCents = 0.10
-// // console.log(`${twentyCents} + ${tenCents} = ${twentyCents + tenCents}`)
-// // // 0.2 + 0.1 = 0.30000000000000004
+// let twentyCents = 0.20
+// let tenCents = 0.10
+// // // console.log(`${twentyCents} + ${tenCents} = ${twentyCents + tenCents}`)
+// // // // 0.2 + 0.1 = 0.30000000000000004
 
-// // We can sometimes avoid this using the toFixed function to force the number of decimal
-// // places as below, but it’s not always useful:
+// // // We can sometimes avoid this using the toFixed function to force the number of decimal
+// // // places as below, but it’s not always useful:
 
-let fixedTwenty = twentyCents.toFixed(2);
-// console.log(fixedTwenty)
+// let fixedTwenty = twentyCents.toFixed(2);
+// // console.log(fixedTwenty)
 
-let fixedTen = tenCents.toFixed(2);
-// console.log(typeof fixedTen)
-// console.log(fixedTwenty + fixedTen) //why is this not working?
+// let fixedTen = tenCents.toFixed(2);
+// // console.log(typeof fixedTen)
+// // console.log(fixedTwenty + fixedTen) //why is this not working?
 
-let float1 = tenCents
-// console.log(float1)
-let float2 = twentyCents
-// console.log(float2)
+// let float1 = tenCents
+// // console.log(float1)
+// let float2 = twentyCents
+// // console.log(float2)
 
-// // Explain why the above code returns the wrong answer
-// // b) Create a function currencyAddition(float1, float2) which safely adds the two
-// // decimal numbers float1 and float2 and returns the correct float result.
-function currencyAddition (float1, float2) {
-  sum = float1 + float2
+// // // Explain why the above code returns the wrong answer
+// // // b) Create a function currencyAddition(float1, float2) which safely adds the two
+// // // decimal numbers float1 and float2 and returns the correct float result.
+// function currencyAddition (float1, float2) {
+//   sum = float1 + float2
 
-  return sum.toFixed(2)
-}
+//   return sum.toFixed(2)
+// }
 
-// console.log( '$', currencyAddition(float1, float2))
+// // console.log( '$', currencyAddition(float1, float2))
 
-// // c) Create a function currencyOperation(float1, float2, operation) which
-// // safely performs the given operation (either +, -, / or *) on the two numbers and returns 
-// // the correct float result. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/switch may be useful.
+// // // c) Create a function currencyOperation(float1, float2, operation) which
+// // // safely performs the given operation (either +, -, / or *) on the two numbers and returns 
+// // // the correct float result. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/switch may be useful.
 
-function currencyOperation(float1, float2, operation) {
-  let result = 0;
-  switch (operation) {
-    case "+":
-      console.log(`${float1} + ${float2}`)
-      result = float1 + float2 //changed from return because added result = 0 to line 185
-      break;
-    case "+":
-      console.log(`${float1} - ${float2}`)
-      result = float1 - float2
-      break;
-    case "+":
-      console.log(`${float1} / ${float2}`)
-      result= float1 / float2
-      break;
-    case "+":
-      console.log(`${float1} * ${float2}`)
-      result = float1 * float2
-      break;
+// function currencyOperation(float1, float2, operation) {
+//   let result = 0;
+//   switch (operation) {
+//     case "+":
+//       console.log(`${float1} + ${float2}`)
+//       result = float1 + float2 //changed from return because added result = 0 to line 185
+//       break;
+//     case "+":
+//       console.log(`${float1} - ${float2}`)
+//       result = float1 - float2
+//       break;
+//     case "+":
+//       console.log(`${float1} / ${float2}`)
+//       result= float1 / float2
+//       break;
+//     case "+":
+//       console.log(`${float1} * ${float2}`)
+//       result = float1 * float2
+//       break;
   
-    default:
-      break;
-  }
-  return result
-}
-// console.log(currencyOperation(0.50, 0.6, '+'))
+//     default:
+//       break;
+//   }
+//   return result
+// }
+// // console.log(currencyOperation(0.50, 0.6, '+'))
 
 
-// // d) (Extension) Extend the above function to include a fourth argument numDecimals
-// // which allows the operation to support different amounts of decimal places from 1 to 10.
-// // HINT: Assume 2 decimal places for b) and c) and use a multiplication factor. 
+// // // d) (Extension) Extend the above function to include a fourth argument numDecimals
+// // // which allows the operation to support different amounts of decimal places from 1 to 10.
+// // // HINT: Assume 2 decimal places for b) and c) and use a multiplication factor. 
 
-function currencyOperation2(float1, float2, operation, numDecimals) {
-  let result = 0;
-  switch (operation) {
-    case "+":
-      console.log(`${float1} + ${float2}`)
-      result = float1 + float2 //changed from return because added result = 0 to line 185
-      break;
-    case "-":
-      console.log(`${float1} - ${float2}`)
-      result = float1 - float2
-      break;
-    case "/":
-      console.log(`${float1} / ${float2}`)
-      result= float1 / float2
-      break;
-    case "*":
-      console.log(`${float1} * ${float2}`)
-      result = float1 * float2
-      break;
+// function currencyOperation2(float1, float2, operation, numDecimals) {
+//   let result = 0;
+//   switch (operation) {
+//     case "+":
+//       console.log(`${float1} + ${float2}`)
+//       result = float1 + float2 //changed from return because added result = 0 to line 185
+//       break;
+//     case "-":
+//       console.log(`${float1} - ${float2}`)
+//       result = float1 - float2
+//       break;
+//     case "/":
+//       console.log(`${float1} / ${float2}`)
+//       result= float1 / float2
+//       break;
+//     case "*":
+//       console.log(`${float1} * ${float2}`)
+//       result = float1 * float2
+//       break;
   
-    default:
-      break;
-  }
-  return result.toFixed(numDecimals)
+//     default:
+//       break;
+//   }
+//   return result.toFixed(numDecimals)
+// }
+// // console.log(currencyOperation2(0.50, 0.6, '+', 5))
+
+
+// // //Test with different values as well as the below:
+// console.log(0.3 === currencyAddition(0.1, 0.2)) // true
+// console.log(0.3 === currencyOperation2(0.1, 0.2, '+', 2)) // true
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// //Number 6...
+// // Create a function unique(duplicatesArray) which takes an array parameter that may
+// // include duplicates. Your function should return an array containing only the unique values
+// // from duplicatesArray.
+// // Test with the following arrays and create another one of your own.
+// const colours = ['red', 'green', 'blue', 'yellow', 'orange', 'red', 'blue', 'yellow']
+// const testScores = [55, 84, 97, 63, 55, 32, 84, 91, 55, 43]
+
+// function unique(duplicatesArray) {
+//   let wordChecker = new Set(duplicatesArray)
+//   return wordChecker
+
+// }
+
+// console.log(unique(colours)) // [ 'red', 'green', 'blue', 'yellow', 'orange' ]
+// console.log(unique(testScores)) // [ 55, 84, 97, 63, 32, 91, 43 ]
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// //Number 7
+// // Use the following array of book objects to practice the array functions for map, find and
+// // filter. Test each of your answers to the below tasks.
+
+const books = [
+{ id: 1, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', year: 1925 },
+{ id: 2, title: 'To Kill a Mockingbird', author: 'Harper Lee', year: 1960 },
+{ id: 3, title: '1984', author: 'George Orwell', year: 1949 },
+{ id: 4, title: 'Brave New World', author: 'Aldous Huxley', year: 1932 },
+{ id: 5, title: 'The Catcher in the Rye', author: 'J.D. Salinger', year: 1951 },
+];
+// console.log(books)
+// const title = books.filter(prod => prod.title).map(prod => prod.title)
+// console.log(title)
+
+// // a) Write a function getBookTitle(bookId) that uses the find function to return the
+// // title of the book object with the matching id.
+function getBookTitle(bookID) { 
+  // let findTitle = books.filter(prod => prod.title).map(prod => prod.title) //not necessary
+  let findTitle = books.find(book => bookID===book.id) //reads through the array items and locates the item's ID compared against the parameter passed through in the function (bookID)
+  // console.log(findTitle)
+  return findTitle.title
 }
-// console.log(currencyOperation2(0.50, 0.6, '+', 5))
+
+// console.log(getBookTitle(3))
+
+// // b) Write a function getOldBooks() that uses the filter function to return all book
+// // objects written before 1950.
+function getOldBooks(writtenDate) {
+  let bookYears = books.filter(year => year.year < writtenDate) //Looks through array and filters all the books that were written before 1950
+  return bookYears
+}
+// console.log(getOldBooks(1950))
+
+// // c) Write a function addGenre() that uses the map function to add a new genre property
+// // to all of the above books, with the value ‘classic’.
+function addGenre(key, value) { //modified past the prompt to add the ability to directly enter in the key:value pairs in the array by calling the function later and passing my parameters through 
+  let bookGenre = books.map((element) => { //mapped array to a new array
+    element.Genre= "Classic"; //defined a new key:value pair
+    const newElement = { //copied array below
+      ...element, 
+      [key]: value, //can pass through parameter to modify new array
+    }
+    return newElement
+  });
+  // console.log(bookGenre)
+  return bookGenre
+}
+// console.log(addGenre("Year Author Died", "Yesterday"))
 
 
-// //Test with different values as well as the below:
-console.log(0.3 === currencyAddition(0.1, 0.2)) // true
-console.log(0.3 === currencyOperation2(0.1, 0.2, '+', 2)) // true
+// // d) (Extension) Write a function getTitles(authorInitial) that uses map and
+// // filter together to return an array of book titles for books written by authors whose
+// // names start with authorInitial.
+function getTitles(authorInitial) {
+  let authInit = books.map(element => element.author == authorInitial)
+  console.log(authInit)
+  // return authInit
+}
+
+// console.log(getTitles("H"))
+
+// // e) (Extension) Write a function latestBook() that uses find and forEach to get the
+// // book with the most recent publication date.
+
