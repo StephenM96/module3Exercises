d414950a2ef1e6d40babfa45e8879835b68d9328
unknown <mossysm96@gmail.com>
2023-04-29 14:43:21 -0500

    working through intermediate

--- javaFundamental.js
+++ javaFundamental.js
@@ -1,4 +1,6 @@
-//Number 1...
+// //Number 1...
+// // What are the results of these expressions? (answer first, then use console.log() to check)
+
 //console.log("" + 1 + 0) //gives 10
 //console.log("" - 1 + 0) //gives -1
 //console.log(true + false) //QUESTION gives nothing (undefined/null/nothing?)
@@ -17,7 +19,11 @@
 //console.log(undefined === null) //QUESTION null is not the same thing as undefined, although it is the same value??
 //console.log(" \t \n" - 2) //line breaks and new text would not affect the integer value
 
-//Number 2...
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// //Number 2...
+// // What are the results of these expressions? (answer first, then use console.log() to check)
+
 //let three = "3"
 //console.log(three)
 //let four = "4"
@@ -39,14 +45,23 @@
 //let lessThan2 = thirty > four
 //console.log(lessThan2) //QUESTION not sure why this gives true???
 
-//Number 3...
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// //Number 3...
+// // Which of the following console.log messages will print? Why?
+
 //if (0) console.log('#1 zero is true') //QUESTION has no value passed through?
 //if ("0") console.log('#2 zero is true')
 //if (null) console.log('null is true') //QUESTION Same as above?
 //if (-1) console.log('negative is true')
 //if (1) console.log('positive is true')
 
-//Number 4...
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// //Number 4...
+// // Rewrite this if using the ternary/conditional operator '?'. Test it with different values for a
+// // and b. What does the ‘+=’ do?
+
 //let a = 2, b = 3;
 //let result = `${a} + ${b} is `;
 
@@ -63,13 +78,33 @@
 
 //console.log(result) // Got it!!!
 
-//Number 5... NOT SURE WHAT TO DO HERE...
-//function getGreeting(name) {
-//return 'Hello ' + name + '!';
-//}
-//console.log(getGreeting)
 
-//Number 6...
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// //Number 5... 
+// // Rewrite the following function using: a) function expression syntax, and b) arrow function
+// // syntax. Test each version to make sure they work the same.
+
+// function getGreeting(name) {
+// return 'Hello ' + name + '!';
+// }
+
+// const getGreeting2 = function(name) {
+// 	return 'Hi ' + name + '!';
+// }
+
+// console.log(getGreeting("Stephen"))
+// console.log(getGreeting2("Stephen"))
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// //Number 6...
+// // a) Complete the inigo object by adding a lastName property and including it in the
+// // greeting.
+// // b) Complete getCatchPhrase so that if the person argument has 6 fingers, it instead
+// // prints his famous catch phrase to the console. HINT: see
+// // https://www.imdb.com/title/tt0093779/characters/nm0001597.
+// // c) Update getCatchPhrase to use arrow function syntax and a conditional operator.
+
 //const westley = {
 //name: 'Westley',
 //numFingers: 6
@@ -97,7 +132,19 @@
 //inigo.greeting(westley)
 //inigo.greeting(rugen)
 
-//Number 7... //Added return this commands
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// //Number 7... //Added return this commands
+// // The following object represents a basketball game and keeps track of the score as the
+// // game progresses.
+// // a) Modify each of the methods so that they can be ‘chained’ together and the last line of
+// // the example code works
+// // b) Add a new method to print the full time final score
+// // c) Add a new object property to keep track of the number of fouls and a method to
+// // increment it, similar but separate to the score. Include the foul count in the half time and
+// // full time console messages
+// // d) Test your object by chaining all the method calls together in different combinations.
+
 //const basketballGame = {
 	//score: 0,
 	//freeThrow() {
@@ -124,7 +171,17 @@
 //modify each of the above object methods to enable function chaining as below:
 //basketballGame.basket().freeThrow().freeThrow().basket().threePointer().halfTime();
 
-//Number 8...
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// //Number 8...
+// // The object below represents a single city.
+// // a) Write a function that takes an object as an argument and uses a for...in loop to access
+// // and print to the console each of those object properties and their values. Test it using
+// // the sydney object below.
+// // b) Create a new object for a different city with different properties and call your function
+// // again with the new object.
+
+
 //const sydney = {
 	//name: 'Sydney',
 	//population: 5121000,
@@ -153,7 +210,19 @@
 	//console.log(`${property}: ${lafayette[property]}`);
 //}
 
-//Number 9... //changed let on original variables to const so they won't be changed
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// //Number 9... //changed let on original variables to const so they won't be changed
+// // Use the following variables to understand how JavaScript stores objects by reference.
+// // a) Create a new moreSports variable equal to teamSports and add some new sport
+// // values to it (using push and unshift)
+// // b) Create a new dog2 variable equal to dog1 and give it a new value
+// // c) Create a new cat2 variable equal to cat1 and change its name property
+// // d) Print the original teamSports, dog1 and cat1 variables to the console. Have they
+// // changed? Why?
+// // e) Change the way the moreSports and cat2 variables are created to ensure the
+// // originals remain independent
+
 //const teamSports = ['Hockey', 'Cricket', 'Volleyball'];
 //const dog1 = 'Bingo';
 //const cat1 = {name: 'Fluffy', breed: 'Siberian'};
@@ -182,7 +251,19 @@
 //console.log(cat1)
 
 
-//Number 10... //not sure how to do classes? Nvm... Got it. Can't figure out the canDrive method though...
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// //Number 10... //not sure how to do classes? Nvm... Got it. Can't figure out the canDrive method though...
+// // The following constructor function creates a new Person object with the given name and
+// // age values.
+// // a) Create a new person using the constructor function and store it in a variable
+// // b) Create a second person using different name and age values and store it in a separate
+// // variable
+// // c) Print out the properties of each person object to the console
+// // d) Rewrite the constructor function as a class called PersonClass and use it to create a
+// // third person using different name and age values. Print it to the console as well.
+// // e) Add a canDrive method to both the constructor function and the class that returns true
+// // if the person is old enough to drive.
+
 //class Person {
 	//constructor(name, age) {
     	//this.name = name;
@@ -204,5 +285,5 @@
 //const david = new Person('David', 28);
 //console.log(david)
 
-//Extension: If you have time, try the JS Challenge Rush at
-//https://www.jschallenger.com/games/rush
+// //Extension: If you have time, try the JS Challenge Rush at
+// //https://www.jschallenger.com/games/rush
--- javaIntermediate.js
+++ javaIntermediate.js
@@ -1,4 +1,8 @@
-//Number 1...
+// //Number 1...
+// // Create a function that takes a string as a parameter and returns the string with the first
+// // character of each word changed into a capital letter, as in the example below. Test it with
+// // different strings.
+
 //function ucFirstLetters(str) {
   //if (typeof str !== 'string') { //checks if the object is a string
     //return ''; //returns a blank value if it is not a string
@@ -13,7 +17,14 @@
 
 //console.log(ucFirstLetters("los angeles")); // Los Angeles
 
-//Number 2...
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// // Number 2...
+// // Create a function truncate(str, max) that truncates a given string of text if its total
+// // length is greater than the max length. It should return either the truncated text, with an
+// // ellipsis (...) added to the end if it was too long, or the original text otherwise.
+// // b) Write another variant of the truncate function that uses a conditional operator.
+
 //function truncate(str, max) {
 	//if(str.length <= max) {
     	//return str;
@@ -21,17 +32,215 @@
         //return str.slice(0, max-3) + '...';
         //}
         
-  //console.log(truncate('This tex will be truncated if it is too long', 5))
+  //console.log(truncate('This text will be truncated if it is too long', 5))
   
-//Number 3... //Did not finish...
-const animals = ['Tiger', 'Giraffe']
-console.log(animals)
-// animals.push ('Elephant', 'Lion');
-// console.log(animals)
-// animals.unshift ('Whales', 'Tigers');
-// console.log(animals)
 
-function replaceMiddleAnimal(newValue) {
-	animals.array[1] = 'Porpoise'
-    }
-    console.log(animals)
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// //Number 3... 
+// // Use the following animals array for the below tasks. Test each one by printing the result to
+// // the console. Review the following link for tips:
+
+// // https://developer.mozilla.org/en-
+// // US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+// // a) Add 2 new values to the end
+// // b) Add 2 new values to the beginning
+// // c) Sort the values alphabetically
+// // d) Write a function replaceMiddleAnimal(newValue) that replaces the value in the
+// // middle of the animals array with newValue
+// // e) Write a function findMatchingAnimals(beginsWith) that returns a new array
+// // containing all the animals that begin with the beginsWith string. Try to make it work
+// // regardless of upper/lower case.
+
+// const animals = ['Tiger', 'Giraffe']
+// // console.log(animals)
+// animals.push ('Elephant', 'Lion', "Aardvark"); //adds 2 animals to the end
+// // console.log(animals)
+// animals.unshift ('Whales', 'Alligator'); //adds 2 animals to the beginning
+// // console.log(animals)
+// const animalsSorted = animals.sort() //alphabetizes the array
+// // console.log(animalsSorted)
+// // console.log(animalsSorted.length /2)
+// // console.log(animalsSorted.splice(2,1))
+
+// function replaceMiddleAnimal(newAnimals) {
+//   let index = Math.floor(animalsSorted.length /2) //converts the middle of the array to the index value
+//   // console.log(newAnimals)
+//   animalsSorted[index] = newAnimals //changes middle array value to "Zebra"
+//   // console.log(animalsSorted)
+// } ;
+
+// replaceMiddleAnimal("TurtleEagle") //calls function and replaces array item with parameter passed in function
+// console.log(animalsSorted)
+
+// replaceMiddleAnimal("Chimera") //same as above
+// console.log(animalsSorted)
+// console.log(animalsSorted)
+
+// const newAnimals = animals.splice() //returns chosen array items in a new array
+// want to replace values in new array from splice and then concat them together with animals array // took care of this
+
+// let titles = products.map(product => product.title)
+// let h2titles = products.map(product => '<h2>'+product.title+'</h2>')
+// let raisedPrices = products.map(product => ({...product, price: product.price + 5}) )
+// console.log(titles) // [ 'Sleeveless Tee', "Men's Hoodie", 'Denim Jeans', 'Ladies Tee' ]
+// console.log(h2titles) // [ '<h2>Sleeveless Tee</h2>', "<h2>Men's Hoodie</h2>", '<h2>Denim
+// Jeans</h2>', '<h2>Ladies Tee</h2>' ]
+// console.log(raisedPrices) // all prices increased by $5
+
+// function findMatchingAnimals(beginsWith) {
+//   // let firstLetter = animalsSorted.filter(item => beginsWith.toLowerCase()===item[0].toLowerCase()) //looking to filter through array items to check first letter in each element
+//   let firstLetter = animalsSorted.filter(item => beginsWith.toLowerCase()===item[0].toLowerCase())
+//   return firstLetter
+//   // console.log(firstLetter)
+// //will need to convert to lower case
+// };
+
+// console.log(findMatchingAnimals("t"))
+// // console.log(animalsSorted)
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// //Number 4...
+// // Write a function camelCase(cssProp) that changes dash-separated CSS properties like
+// // 'margin-left' into camel-cased 'marginLeft'.
+// // The function should remove all dashes, and uppercase the first letter of each word after a
+// // dash.
+// // b) Create variants of the camelCase function that use different types of for loops, and
+// // c) with and without the conditional operator.
+
+
+// let cssProperties = ["text-decoration", "margin-left", 'font-size', "border"] //created an array to modify
+// // console.log(cssProperties[0].split("-")) //converts an array item to string and splits at hyphen
+
+// let camelCaseProperties = cssProperties.map(camelCase); //called camelCase function and mapped it in an array
+
+// // console.log(cssProperties.map(item => {
+// //   console.log(item) 
+// //   return item
+// // }))
+// // let titles = products.map(product => product.title) //for example use
+
+// function camelCase(cssProp) {
+//   //sort through  CSS properties and split them if they have dashes.
+//   let words = cssProp.split('-')
+
+
+//   //upper case the first letter of each word after immediately after a dash
+//   for (let i = 1; i < words.length; i++) {
+//     words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+//   }
+//   return words.join('')
+// }
+
+// // camelCase(cssProperties)
+// // console.log(cssProperties.length)
+
+// // camelCase(cssProperties)
+// // console.log(cssProperties)
+
+// console.log(camelCaseProperties) //call function
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// // //Number 5
+// // Decimal number operations in JavaScript can lead to unexpected results, as in the
+// // following:
+
+let twentyCents = 0.20
+let tenCents = 0.10
+// // console.log(`${twentyCents} + ${tenCents} = ${twentyCents + tenCents}`)
+// // // 0.2 + 0.1 = 0.30000000000000004
+
+// // We can sometimes avoid this using the toFixed function to force the number of decimal
+// // places as below, but it’s not always useful:
+
+let fixedTwenty = twentyCents.toFixed(2);
+// console.log(fixedTwenty)
+
+let fixedTen = tenCents.toFixed(2);
+// console.log(typeof fixedTen)
+// console.log(fixedTwenty + fixedTen) //why is this not working?
+
+let float1 = tenCents
+// console.log(float1)
+let float2 = twentyCents
+// console.log(float2)
+
+// // Explain why the above code returns the wrong answer
+// // b) Create a function currencyAddition(float1, float2) which safely adds the two
+// // decimal numbers float1 and float2 and returns the correct float result.
+function currencyAddition (float1, float2) {
+  sum = float1 + float2
+
+  return sum.toFixed(2)
+}
+
+// console.log( '$', currencyAddition(float1, float2))
+
+// // c) Create a function currencyOperation(float1, float2, operation) which
+// // safely performs the given operation (either +, -, / or *) on the two numbers and returns 
+// // the correct float result. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/switch may be useful.
+
+function currencyOperation(float1, float2, operation) {
+  let result = 0;
+  switch (operation) {
+    case "+":
+      console.log(`${float1} + ${float2}`)
+      result = float1 + float2 //changed from return because added result = 0 to line 185
+      break;
+    case "+":
+      console.log(`${float1} - ${float2}`)
+      result = float1 - float2
+      break;
+    case "+":
+      console.log(`${float1} / ${float2}`)
+      result= float1 / float2
+      break;
+    case "+":
+      console.log(`${float1} * ${float2}`)
+      result = float1 * float2
+      break;
+  
+    default:
+      break;
+  }
+  return result
+}
+// console.log(currencyOperation(0.50, 0.6, '+'))
+
+
+// // d) (Extension) Extend the above function to include a fourth argument numDecimals
+// // which allows the operation to support different amounts of decimal places from 1 to 10.
+// // HINT: Assume 2 decimal places for b) and c) and use a multiplication factor. 
+
+function currencyOperation2(float1, float2, operation, numDecimals) {
+  let result = 0;
+  switch (operation) {
+    case "+":
+      console.log(`${float1} + ${float2}`)
+      result = float1 + float2 //changed from return because added result = 0 to line 185
+      break;
+    case "-":
+      console.log(`${float1} - ${float2}`)
+      result = float1 - float2
+      break;
+    case "/":
+      console.log(`${float1} / ${float2}`)
+      result= float1 / float2
+      break;
+    case "*":
+      console.log(`${float1} * ${float2}`)
+      result = float1 * float2
+      break;
+  
+    default:
+      break;
+  }
+  return result.toFixed(numDecimals)
+}
+// console.log(currencyOperation2(0.50, 0.6, '+', 5))
+
+
+// //Test with different values as well as the below:
+console.log(0.3 === currencyAddition(0.1, 0.2)) // true
+console.log(0.3 === currencyOperation2(0.1, 0.2, '+', 2)) // true
